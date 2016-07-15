6f46559d51c8855caca72538935b295e1ee8af98
Rafael Zamora <rz4@hood.edu>
2016-07-15 17:48:04 -0400

    Bug Fix 1

--- PyAide/__init__.py
+++ PyAide/__init__.py
@@ -1,1 +1,7 @@
 from PyAide.Enviros.Enviro import Enviro
+from PyAide.Agents.Agent import Agent
+from PyAide.Enviros.CustomEnviros.BoardEnviro import BoardEnviro
+from PyAide.Enviros.CustomEnviros.MazeEnviro import MazeEnviro
+from PyAide.Enviros.CustomEnviros.EightPuzzleEnviro import EightPuzzleEnviro
+from PyAide.Agents.CustomAgents.RandomAgent import RandomAgent
+from PyAide.Tools.AIDEGUI import AIDEGUI
