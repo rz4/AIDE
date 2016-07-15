c32371b620403622d2b154fb6adee4651d559069
Rafael Zamora <rz4@hood.edu>
2016-07-15 17:42:03 -0400

    Bug Fix 1

--- PyAide/__init__.py
+++ PyAide/__init__.py
@@ -1,13 +1,1 @@
-import types
 
-for key, obj in PyAide.__dict__.iteritems():
-    if type(obj) is types.ModuleType:
-        print(key)
-
-from Enviros.Enviro import Enviro
-from Agents.Agent import Agent
-from Enviros.CustomEnviros.BoardEnviro import BoardEnviro
-from Enviros.CustomEnviros.MazeEnviro import MazeEnviro
-from Enviros.CustomEnviros.EightPuzzleEnviro import EightPuzzleEnviro
-from Agents.CustomAgents.RandomAgent import RandomAgent
-from Tools.AIDEGUI import AIDEGUI
