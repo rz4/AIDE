2880334a0ffac58d6a1db1db948abc5e3440ee6b
Rafael Zamora <rz4@hood.edu>
2016-07-15 17:21:18 -0400

    Bug Fix 1

--- PyAide/__init__.py
+++ PyAide/__init__.py
@@ -1,10 +1,10 @@
 import pkgutil
 print([name for _, name, _ in pkgutil.iter_modules(['testpkg'])])
 
-from .Enviros.Enviro import Enviro
-from .Agents.Agent import Agent
-from .Enviros.CustomEnviros.BoardEnviro import BoardEnviro
-from .Enviros.CustomEnviros.MazeEnviro import MazeEnviro
-from .Enviros.CustomEnviros.EightPuzzleEnviro import EightPuzzleEnviro
-from .Agents.CustomAgents.RandomAgent import RandomAgent
-from .Tools.AIDEGUI import AIDEGUI
+from Enviros.Enviro import Enviro
+from Agents.Agent import Agent
+from Enviros.CustomEnviros.BoardEnviro import BoardEnviro
+from Enviros.CustomEnviros.MazeEnviro import MazeEnviro
+from Enviros.CustomEnviros.EightPuzzleEnviro import EightPuzzleEnviro
+from Agents.CustomAgents.RandomAgent import RandomAgent
+from Tools.AIDEGUI import AIDEGUI
